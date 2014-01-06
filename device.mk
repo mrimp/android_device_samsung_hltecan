7083ce89efba45f82abc97997ebd272838c00fcd
slayher <layhertony@gmail.com>
2014-01-05 22:31:34 -0500

    Add define for NFC
    Change-Id: Ibb0eeca87e0f94a2b78c5dc22287c33f891fb040

--- device.mk
+++ device.mk
@@ -17,6 +17,9 @@
 ## (2) Also get non-open-source specific aspects if available
 $(call inherit-product-if-exists, vendor/samsung/hltecan/hltecan-vendor.mk)
 
+#Specify NFC type.
+TARGET_NFC_TECH := nxp
+
 ## common overlays
 DEVICE_PACKAGE_OVERLAYS += device/samsung/hlte-common/overlay-gsm
 
